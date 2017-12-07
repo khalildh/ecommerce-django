7a614526effc039431143c3b009b1e5d66ab9b23
Khalil Hughes <hughes.khalil@gmail.com>
2017-12-07 16:29:42 -0500

    Finished Custom User Model

--- ecommerce/src/config/settings.py
+++ ecommerce/src/config/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'orders',
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
